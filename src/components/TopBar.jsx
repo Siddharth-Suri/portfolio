7481e6a47a0433a523614d95f8fed0a3e81b7017
Siddharth Suri <siddharth2005suri@gmail.com>
2025-01-21 20:08:01 +0530

    added transition to coffee

--- src/components/TopBar.jsx
+++ src/components/TopBar.jsx
@@ -2,10 +2,14 @@ import { useNavigate } from "react-router-dom";
 import { colourTheme } from "../../state/Theme";
 import { useRecoilState, useRecoilValue } from "recoil";
 import { ThemeButton } from "./themeButton";
+import { HoverMechanic } from "../../state/HoveringMechanic";
 import { memo } from "react";
+
 export const TopBar = memo(() => {
     const currentTheme = useRecoilValue(colourTheme);
+    const [currentHover, setCurrentHover] = useRecoilState(HoverMechanic);
     const navigate = useNavigate();
+
     return (
         <div
             className={`flex p-4 font-custom justify-around items-center text-xl gap-2 font-semibold ${
@@ -29,12 +33,25 @@ export const TopBar = memo(() => {
                 </div>
             </div>
 
-            <div className="flex p-4 ce">
+            <div className="flex p-4">
                 <div className="p-4 cursor-pointer hover:text-yellow-400 hover:underline">
-                    <ThemeButton></ThemeButton>
+                    <ThemeButton />
                 </div>
-                <div className="p-4 cursor-pointer hover:text-yellow-400 hover:underline">
-                    Coffeee
+                <div
+                    className="flex cursor-pointer p-4 hover:text-yellow-400"
+                    onMouseEnter={() => setCurrentHover(true)}
+                    onMouseLeave={() => setCurrentHover(false)}
+                >
+                    <div
+                        className={`overflow-hidden whitespace-nowrap transition-all ease-in-out duration-1000 ${
+                            currentHover
+                                ? "max-w-[200px] opacity-100"
+                                : "max-w-0 opacity-0"
+                        }`}
+                    >
+                        Buy me a
+                    </div>
+                    <div className="pl-3 hover:underline">Coffeee</div>
                 </div>
             </div>
         </div>
---
+++ state/HoveringMechanic.jsx
@@ -0,0 +1,6 @@
+import { atom } from "recoil";
+
+export const HoverMechanic = atom({
+    key: "HoverMechanic",
+    default: "false",
+});
